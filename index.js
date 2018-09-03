eabd2f876542a9eaf4a36116843cde720583303d
sailplaneTW <sailplaneTW@gmail.com>
2018-09-03 11:34:52 +0800

    .

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+const port = 3000
+
+files = []
+
+fs.readdir('./public/image', (err, _files) => {
+    files = [..._files]
+    console.log('!!!', JSON.stringify(files))
+    /*
+  files.forEach(file => {
+    console.log(file);
+  });
+  */
+})
+
+app.get('/', (request, response) => {
+    response.send(
+        files.reduce((acc, v) => {
+            return acc+`<img src='./image/${v}' />`
+        }, '')
+    )
+})
+
+app.use(express.static('public'));
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
